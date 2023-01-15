287b0f79d48108f49a151776e5a56df3ba81fecb
Mitch Ludkey <mludk92@hotmail.com>
2023-01-14 19:41:16 -0600

    answered up to strech goals

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -45,12 +45,24 @@ else {console.log("How about we stay home?")}
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
+if (luckyNumber === 2 && adventurous === true){
+    console.log("Roll the dice!")
+};
+
 
 // 15 - Write a conditional that console logs "I can have more pets!" 
 //if the value of `pets` is less than the value of `allowedPets`,
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
+if (pets < allowedPets){
+    console.log("I can have more pets")}
+    else if (pets === allowedPets){ 
+        console.log("I have enough pets")
+    }
+    else if (pets > allowedPets) 
+    {console.log("Oh No, I have too many pets!")};
+
 
 // STRETCH GOALS:
 
